0756fde303e514564c0fa67ed7944fe6cb9de4c6
jiaxianyan <91183609+jiaxianyan@users.noreply.github.com>
2026-01-08 20:46:39 +0800

    Update utils.py

--- BioMiner/dataset/utils.py
+++ BioMiner/dataset/utils.py
@@ -1,9 +1,5 @@
 import os
 
-prompt_dict = {'gpt-4o':'gpt-series-prompt',
-               'gpt-4o-mini':'gpt-series-prompt',
-               'gpt-4.1-mini':'gpt-series-prompt'}
-
 structure_prompt_strategy = {'origin': '',
                              'origin-with-box-index': '_with_index',
                              'updated-with-box-index': '_updated_with_index',
@@ -16,6 +12,12 @@ bioactivity_prompt_strategy = {'origin': '',
                                 'updated_v3': '_updated_v3'
                                 }
 
+def get_prompt_series(mllm):
+    if 'gpt' in mllm.lower():
+        return 'gpt-series-prompt'
+
+    else:
+        return 'gemini-series-prompt'
 
 def load_prompts(prompt_path='BioMiner/commons/prompts', 
                  text_mllm='local-biominer-instruct', 
@@ -24,11 +26,19 @@ def load_prompts(prompt_path='BioMiner/commons/prompts',
                  bioactivity_image_strategy='updated', 
                  structure_full_strategy='updated-with-box-index', 
                  structure_part_strategy='updated-with-box-index'):
-    bioactivity_text_prompt_path = os.path.join(prompt_path, prompt_dict.get(text_mllm, 'gemini-series-prompt'), f'prompt_for_text{bioactivity_prompt_strategy[bioactivity_text_strategy]}.txt')
-    bioactivity_image_prompt_path = os.path.join(prompt_path, prompt_dict.get(vision_mllm, 'gemini-series-prompt'), f'prompt_for_image{bioactivity_prompt_strategy[bioactivity_image_strategy]}.txt')
-    structure_full_prompt_path = os.path.join(prompt_path, prompt_dict.get(vision_mllm, 'gemini-series-prompt'), f'prompt_for_structure_full{structure_prompt_strategy[structure_full_strategy]}.txt')
-    structure_part_prompt_path = os.path.join(prompt_path, prompt_dict.get(vision_mllm, 'gemini-series-prompt'), f'prompt_for_structure_part{structure_prompt_strategy[structure_part_strategy]}.txt')
-    merge_prompt_path = os.path.join(prompt_path, prompt_dict.get(text_mllm, 'gemini-series-prompt'), 'prompt_for_merging_without_ligand_structure.txt')
+
+
+    bioactivity_text_prompt_path = os.path.join(prompt_path, get_prompt_series(text_mllm), f'prompt_for_text{bioactivity_prompt_strategy[bioactivity_text_strategy]}.txt')
+    bioactivity_image_prompt_path = os.path.join(prompt_path, get_prompt_series(vision_mllm), f'prompt_for_image{bioactivity_prompt_strategy[bioactivity_image_strategy]}.txt')
+    structure_full_prompt_path = os.path.join(prompt_path, get_prompt_series(vision_mllm), f'prompt_for_structure_full{structure_prompt_strategy[structure_full_strategy]}.txt')
+    structure_part_prompt_path = os.path.join(prompt_path, get_prompt_series(vision_mllm), f'prompt_for_structure_part{structure_prompt_strategy[structure_part_strategy]}.txt')
+    merge_prompt_path = os.path.join(prompt_path, get_prompt_series(text_mllm), 'prompt_for_merging_without_ligand_structure.txt')
+
+    print(bioactivity_text_prompt_path)
+    print(bioactivity_image_prompt_path)
+    print(structure_full_prompt_path)
+    print(structure_part_prompt_path)
+    print(merge_prompt_path)
 
     with open(bioactivity_text_prompt_path, 'r') as f:
         bioactivity_text_prompt = f.read().strip()
